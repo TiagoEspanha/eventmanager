8bc204e53335e315b40b06845380268e436ad001
Tiago Espanha <tiago.espanha@gmail.com>
2023-07-16 22:13:01 -0300

    add rota para adicionar organizador em edicao

--- src/main/java/com/tiagoespanha/eventmanager/controllers/EdicaoController.java
+++ src/main/java/com/tiagoespanha/eventmanager/controllers/EdicaoController.java
@@ -1,6 +1,7 @@
 package com.tiagoespanha.eventmanager.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -43,7 +44,7 @@ public class EdicaoController {
             }
 
             novaEdicao.setCriador(usuarioAtual);
-            System.out.println(novaEdicao);
+
             Evento evento; 
             try {
                 evento = eventoRepository.findById(novaEdicao.getEvento().getId()).orElseThrow();
@@ -58,4 +59,49 @@ public class EdicaoController {
 
         }
 
+
+    @PostMapping("{edicaoId}/organizador/{organizadorId}")
+    public String criarEdicao(
+        @RequestHeader("usuarioId") Long usuarioId,
+        @PathVariable Long edicaoId,
+        @PathVariable Long organizadorId) {
+
+            if (usuarioId == organizadorId) {
+                return "O organizador deve ser um usuário diferente do admin";
+            }
+
+            Usuario usuarioAtual;
+            try {
+                usuarioAtual = usuarioRepository.findById(usuarioId).orElseThrow();
+            }
+            catch(Exception err) {
+                return "Usuário não encontrado";
+            }
+        
+            if (usuarioAtual == null || usuarioAtual.getAdmin() == false) {
+                return "Usuário precisa ser administrador";
+            }
+
+            Edicao edicao;
+            try {
+                edicao = edicaoRepository.findById(edicaoId).orElseThrow();
+            }
+            catch(Exception err) {
+                return "Edicao do evento não existe";
+            }
+
+
+            Usuario organizador;
+            try {
+                organizador = usuarioRepository.findById(organizadorId).orElseThrow();
+            }
+            catch(Exception err) {
+                return "Usuário marcado como organizador da edicao não existe";
+            }
+
+            edicao.setOrganizador(organizador);
+            edicaoRepository.save(edicao);
+
+            return "sucesso";
+    }
 }
